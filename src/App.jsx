3a62212f321e582e942a32e4c384511bcd907f53
BE NGOC HOA <hoangocbe@lgcns.com>
2024-10-14 16:38:36 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css'
-import UserCard from './components/UserCard'
+import UserCard from './components/UserCard/UserCard'
 import axios from 'axios';
 // import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -37,7 +37,24 @@ function App() {
   };
 
   useEffect(() => {
-    fetchUsers();
+    // fetchUsers();
+    const storedUsers=getStoredUsers();
+    if(storedUsers.length > 0){
+      setUsers(storedUsers);
+      setLoading(false);
+    } else {
+      axios.get('https://randomuser.me/api/?results=10')
+      .then(response => {
+        const newUsers=response.data.results;
+        setUsers(newUsers);
+        saveUsersToStorage(newUsers);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      })
+    }
   }, []);
 
   if (loading) {
@@ -49,6 +66,7 @@ function App() {
   }
 
   console.log(users);
+
   return (
     <div className="app">
       <h1>User Cards</h1>
@@ -63,3 +81,17 @@ function App() {
 }
 
 export default App
+
+const getStoredUsers = () => {
+  const storedUsers=localStorage.getItem('users');
+  return storedUsers ? JSON.parse(storedUsers) : [];
+}
+
+const saveUsersToStorage = (users) => {
+  localStorage.setItem('users',JSON.stringify(users));
+}
+
+const updateUsers = (updateUsers) => {
+  setUsers(updateUsers);
+  saveUsersToStorage(updateUsers);
+}
